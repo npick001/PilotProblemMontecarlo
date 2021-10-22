8754e266bad4234a1af364e11ef78edac42d9f01
npick001 <npick001@odu.edu>
2021-10-22 16:15:36 -0400

    Cleaned up normal distribution generation.

--- PilotSim.h
+++ PilotSim.h
@@ -1,2 +1,12 @@
 
-void generateRandPoints(stdevX, stdevY, randX, randY);
+void generateRandPoints(double stdevX, double stdevY, double randX, double randY);
+double getRand(double stdevX);
+struct PilotSim{
+    double mean = 0;
+    double stdevX = 500.000;
+    double stdevY = 250.000;
+    double randX[trialSize];
+    double randY[trialSize]; 
+    double normalX[trialSize]; 
+    double normalY[trialSize];
+};
